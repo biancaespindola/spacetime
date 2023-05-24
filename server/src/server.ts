edf8b6b1fec5189b96409eaf45f018d656c86e45
Bianca Espíndola <32000570+BiancaEspindola@users.noreply.github.com>
2023-05-24 22:09:08 +0200

    instalando fastfy

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,15 @@
-console.log("it's time");
+import fastify from 'fastify';
+
+const app = fastify();
+
+//HTTP methods: GET POST PUT DELETE PATCH
+
+app.get('/hello', () => {
+	return 'Good morning!';
+});
+
+app.listen({
+	port: 3333,
+}).then(() => {
+	console.log('Server listening on port 3333 😊');
+});
