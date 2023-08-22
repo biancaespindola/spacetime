cf9688bdaf4ee3dffef3b4af0158ffe8d4fb6dac
Bianca Espíndola <32000570+BiancaEspindola@users.noreply.github.com>
2023-08-22 21:18:29 +0200

    initial mobile config 1:27:40

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,26 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
-	return (
-		//TODO: Fazer funcionar o tailwind no react native
-		<View style={styles.container} className="bg-gray-900 flex-1">
-			<Text style={styles.text}>Spacetime</Text>
+  return (
+    // TODO: Fazer funcionar o tailwind no react native
+    <View style={styles.container} className=" flex-1 items-center justify-center bg-gray-900">
+      <Text style={styles.text} className='text-5xl font-bold text-gray-50'>Spacetime</Text>
 
-			<StatusBar style="auto" />
-		</View>
-	);
+      <StatusBar style="light" translucent />
+    </View>
+  )
 }
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#09090b',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	text: {
-		color: '#fafafa',
-		fontWeight: '900',
-		fontSize: 64,
-	},
-});
+  container: {
+    flex: 1,
+    backgroundColor: '#09090b',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#f9fafb',
+    fontWeight: '900',
+    fontSize: 56,
+  },
+})
