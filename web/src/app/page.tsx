2b9e241b13c05804f1c4fbea963ac87f79b1c40c
Bianca Espíndola <32000570+BiancaEspindola@users.noreply.github.com>
2023-08-23 22:21:15 +0200

    font config 10:28

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
-	return (
-		<div className="h-screen bg-zinc-900 text-zinc-50 p-6">
-			<h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
-		</div>
-	);
+  return (
+    <div className="h-screen bg-zinc-900 text-zinc-50 p-6">
+      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+      <h1 className="text-4xl font-bold font-alt">Sua cápsula do tempo</h1>
+    </div>
+  );
 }
