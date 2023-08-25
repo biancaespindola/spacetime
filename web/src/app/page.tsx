a951e4fd8f258f165cffa9890501800e83638939
Bianca Espíndola <32000570+BiancaEspindola@users.noreply.github.com>
2023-08-25 11:33:31 +0200

    background home page 35:20

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,11 +1,28 @@
 export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
-      <div>
-        <h1>Spacetime</h1>
-        <p>Sua cápsula do tempo</p>
+      {/* Left */}
+      <div className="relative flex flex-col items-start justify-between  overflow-hidden border-r border-white/10 bg-[url('../assets/bg-stars.svg')] bg-cover px-28 py-16">
+        {/* Blur */}
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+
+        {/* Stripes */}
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+        <h1>Bom dia</h1>
+        <h1>Bom dia</h1>
+        <h1>Bom dia</h1>
       </div>
-      <div></div>
+      <div className="flex flex-col bg-[url('../assets/bg-stars.svg')] bg-cover p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{" "}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora
+            </a>
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
