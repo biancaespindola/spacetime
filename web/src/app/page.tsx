f16f1724a1c68fa856fc11a349a8d35a5a632b05
Bianca Espíndola <32000570+BiancaEspindola@users.noreply.github.com>
2023-08-29 11:29:27 +0200

    add Hero component at page  45:44

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,6 @@
+import Logo from "../assets/logo.svg";
+import Image from "next/image";
+
 import { User } from "lucide-react";
 export default function Home() {
   return (
@@ -10,13 +13,48 @@ export default function Home() {
         {/* Stripes */}
         <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
 
-        {/* Elements */}
-        <a href="flex items-center gap-3 text-left">
+        {/* Sing In */}
+        <a
+          href=""
+          className="flex items-center gap-3 text-left hover:text-gray-50 transition-colors"
+        >
           <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
             <User className="h-5 w-5 text-gray-500" />
           </div>
+          <p className="max-w-[140px] text-sm leading-snug ">
+            {" "}
+            <span className="underline">Crie sua conta</span> e salve suas
+            memórias
+          </p>
         </a>
+
+        {/* Hero */}
+        <div className="space-y-5">
+          <Image src={Logo} alt="Logo Spacetime" />
+
+          <div className="max-w-[420px] space-y-4">
+            <h1 className="text-5xl font-bold leading-tight text-gray-50">
+              Sua cápsula do tempo
+            </h1>
+            <p className="text-lg leading-relaxed">
+              Colecione momentos marcantes da sua jornada e compartilhe (se
+              quiser) com o mundo!
+            </p>
+          </div>
+
+          <a
+            href=""
+            className="uppercase inline-block rounded-full text-sm font-alt leading-none bg-green-500 px-5 py-3 text-black hover:bg-green-600"
+          >
+            Compartilhar lembrança
+          </a>
+        </div>
+
+        {/* Page */}
+        <div>Feito com amor por Bianca</div>
       </div>
+
+      {/* Right */}
       <div className="flex flex-col bg-[url('../assets/bg-stars.svg')] bg-cover p-16">
         <div className="flex flex-1 items-center justify-center">
           <p className="w-[360px] text-center leading-relaxed">
