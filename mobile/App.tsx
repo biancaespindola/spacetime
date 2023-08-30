01021616a7d4050262896e11e913b8a3624a302f
Bianca Espíndola <32000570+BiancaEspindola@users.noreply.github.com>
2023-08-30 21:55:02 +0200

    initial config font mobile 53:45

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,25 +1,59 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import {
+  BaiJamjuree_700Bold,
+} from '@expo-google-fonts/bai-jamjuree'
+
+
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
     // TODO: Fazer funcionar o tailwind no react native
     <View style={styles.container} className=" flex-1 items-center justify-center bg-gray-900">
-      <Text style={styles.text} className='text-5xl font-bold text-gray-50'>Spacetime</Text>
+      <Text style={styles.title } className='text-5xl font-bold text-gray-50'>Spacetime</Text>
+      <Text style={styles.alt } className='text-2xl font-bold text-gray-50'>Uma viagem no tempo</Text>
 
       <StatusBar style="light" translucent />
     </View>
   )
 }
 const styles = StyleSheet.create({
+  
+  body:{
+    fontFamily: 'Roboto_400Regular',
+  },
+  alt:{
+    fontFamily: 'BaiJamjuree_700Bold',
+    color: '#eaeaea',
+    fontWeight: '200',
+    fontSize: 30,
+  },
+
   container: {
     flex: 1,
-    backgroundColor: '#09090b',
+    backgroundColor: '#121215',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
-    color: '#f9fafb',
+  title: {
+    fontFamily: 'BaiJamjuree_700Bold',
+    color: '#eaeaea',
     fontWeight: '900',
     fontSize: 56,
   },
